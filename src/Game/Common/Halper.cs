f22e5732bc33d918a47c631f5f3806d18b65694f
Maxsud Mutalov <maxsudmutalov@gmail.com>
2024-09-16 20:08:42 +0500

    Initial commit

---
+++ src/Game/Common/Halper.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+
+namespace Game.Common;
+
+public static class Halper
+{
+    private static readonly StringBuilder stringBuilder = new();
+    private static Int32 half;
+    private const String title = @"↓ PC\User →";
+
+    public static void DisplayHalpTable(String[] inputs)
+    {
+        half = CalculateHalfOfInputs(inputs.Length);
+
+        stringBuilder.Append($"+{new String('-', 13)}+");
+
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            stringBuilder.Append($"{new String('-', inputs[i].Length + 2)}+");
+        }
+
+        stringBuilder.Append($"\n| {title} | ");
+        stringBuilder.AppendJoin(" | ", inputs);
+        stringBuilder.Append($" |\n+{new String('-', 13)}+");
+
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            stringBuilder.Append($"{new String('-', inputs[i].Length + 2)}+");
+        }
+
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            stringBuilder.Append(
+                $"\n| " +
+                $"{inputs[i]}" +
+                $"{new String(' ', 13 - (inputs[i].Length + 1))}|");
+
+            for (int j = 0; j < inputs.Length; j++)
+            {
+                if (i == j)
+                {
+                    stringBuilder.Append(
+                        $" Draw" +
+                        $"{new String(' ', (inputs[j].Length + 2) - 5)}");
+                }
+                else if (i + 1 < j + 1 && j + 1 <= (i + 1) + half)
+                {
+                    stringBuilder.Append(
+                        $" Win" +
+                        $"{new String(' ', (inputs[j].Length + 2) - 4)}");
+                }
+                else
+                {
+                    stringBuilder.Append(
+                        $" Lose" +
+                        $"{new String(' ', (inputs[j].Length + 2) - 5)}");
+                }
+                stringBuilder.Append('|');
+            }
+            stringBuilder.Append($"\n+{new String('-', 13)}+");
+            for (int k = 0; k < inputs.Length; k++)
+            {
+                stringBuilder.Append($"{new String('-', inputs[k].Length + 2)}+");
+            }
+        }
+    }
+
+    private static Int32 CalculateHalfOfInputs(Int32 lengthOfInputs)
+    {
+        Int32 half = (lengthOfInputs - 1) / 2;
+
+        return half;
+    }
+}
---
+++ src/Game/Common/Rules.cs
@@ -0,0 +1,17 @@
+﻿namespace Game.Common;
+
+public static class Rules
+{
+    public const String RulesOfGame = "Rules of game:\n" +
+        "\t1. User must input an odd number (≥ 3) non-repeating strings\n" +
+        "\t2. These passed strings are moves (for example, Rock Paper \n" +
+        "\t   Scissors or Rock Paper Scissors Lizard Spock or 1 2 3 4 5 6 7 8 9 \n" +
+        "\t   or A B C D E F G etc.)\n" +
+        "\t3. Half of the next moves in the circle wins, half of the \n" +
+        "\t   previous moves in the circle lose (the semantics of the strings-moves \n" +
+        "\t   is not important, computer and user play by the rules build upon the \n" +
+        "\t   moves order the user used, even if the stone loses to scissors in its \n" +
+        "\t   order—the contents of the strings-moves are not important).\n" +
+        "\t4. For understanding scheme look at the table below\n" +
+        "GOOD LUCK MY FRIEND!!!";
+}
